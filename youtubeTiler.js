54ec1c4c681ef11e85aa4b88762b24fa0e62fa47
ellisborealis <townsend08002@comcast.net>
2016-06-20 23:56:13 -0400

    - added jQuery-contextMenu to the project  - context menu now exists when you right click the red bar, you can move videos to the four corners or full screen

--- youtubeTiler.js
+++ youtubeTiler.js
@@ -1,4 +1,13 @@
 $(function() {
+	$(window).resize(function(){
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		
+		var areaHeight = Math.min(windowWidth * (1080/1920),windowHeight);
+		
+		$("#main").height(areaHeight);
+	});
+
 	$("#addButton").click(function () {
 		var directValue = $("#urlBox").val();
 		var youtubeId = YouTubeGetID(directValue);
@@ -7,9 +16,9 @@ $(function() {
 			return;
 		}
 		var codeString = '';
-		codeString = codeString.concat('<div class="youtubePane" style="width:800px;height:450px;left:200px;top:200px;"><div class="hoverDiv"><button type="button" class="closeButton">X</button></div><div class="mask"></div><iframe style="width:100%; height:100%;" src="https://www.youtube.com/embed/');
+		codeString = codeString.concat('<div class="youtubePane" style="width:800px;height:450px;left:200px;top:200px;"><div class="hoverDiv context-menu-one btn btn-neutral"><button type="button" class="closeButton">X</button></div><div class="mask"></div><iframe style="width:100%; height:100%;" src="https://www.youtube.com/embed/');
 		codeString = codeString.concat(youtubeId);
-		codeString = codeString.concat('" frameborder="0" allowfullscreen></iframe></div>');
+		codeString = codeString.concat('" frameborder="0"></iframe></div>');
 		$("#main").append(codeString);
 	});
 
@@ -28,6 +37,40 @@ $(function() {
 		.mousedown(function() {
 			this.parentNode.parentNode.remove();
 		});
+		
+        $.contextMenu({
+            selector: '.context-menu-one', 
+            callback: function(key, options) {
+                if(key == "bl")
+				{
+					this.parent().css({'width': '50%', 'height': '50%', 'top': 'auto', 'right': 'auto', 'bottom': '0px', 'left': '0px'});
+				}
+                if(key == "br")
+				{
+					this.parent().css({'width': '50%', 'height': '50%', 'top': 'auto', 'right': '0px', 'bottom': '0px', 'left': 'auto'});
+				}
+                if(key == "tl")
+				{
+					this.parent().css({'width': '50%', 'height': '50%', 'top': '0px', 'right': 'auto', 'bottom': 'auto', 'left': '0px'});
+				}
+                if(key == "tr")
+				{
+					this.parent().css({'width': '50%', 'height': '50%', 'top': '0px', 'right': '0px', 'bottom': 'auto', 'left': 'auto'});
+				}
+                if(key == "fs")
+				{
+					this.parent().css({'width': '100%', 'height': '100%', 'top': 'auto', 'right': 'auto', 'bottom': '0px', 'left': '0px'});
+				}
+            },
+            items: {
+                "bl": {name: "Bottom Left"},
+                "br": {name: "Bottom Right"},
+                "tl": {name: "Top Left"},
+                "tr": {name: "Top Right"},
+                "sep1": "---------",
+                "fs": {name: "Full Screen"}
+            }
+        });
 	});
 });
 
