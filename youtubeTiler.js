d72103b90d124c3af965f0ad8306991c111fc67e
ellisborealis <townsend08002@comcast.net>
2016-06-21 07:14:09 -0400

    - all code i wrote is now seperate from code i'm using  - added pause all videos button (just removes the src and adds it back)  - fxied the z-index on the close button so I can use "click" instead of "mousedown"

--- youtubeTiler.js
+++ youtubeTiler.js
@@ -1,13 +1,28 @@
 $(function() {
+	/* when window is resized also resize the main according to the aspect ratio of a 16:9 video */
 	$(window).resize(function(){
 		var windowWidth = $(window).width();
 		var windowHeight = $(window).height();
 		
-		var areaHeight = Math.min(windowWidth * (1080/1920),windowHeight);
+		var areaHeight = Math.min(windowWidth * (9/16),windowHeight);
 		
 		$("#main").height(areaHeight);
 	});
 
+	/* add pause code */
+	$("#pauseButton").click(function() {
+		$("iframe").each(function (){
+			var video = $(this).attr("src");
+
+			$(this).attr("src","");
+			$(this).attr("src",video);
+		});
+	});
+	
+	/* move pause button on start of document */
+	$("#pauseButton").css({"left": parseInt($("#addButton").css("left"),10) + $("#addButton").width() + 5});
+
+	/* add video code */
 	$("#addButton").click(function () {
 		var directValue = $("#urlBox").val();
 		var youtubeId = YouTubeGetID(directValue);
@@ -22,86 +37,58 @@ $(function() {
 		$("#main").append(codeString);
 	});
 
-	<!-- dynamically generated elemnts work now http://stackoverflow.com/questions/6658752/click-event-doesnt-work-on-dynamically-generated-elements -->
+	/* run this code AFTER every button press */
 	$( "#addButton" ).on("click", function() {
 		$( "div.youtubePane" )
-		<!-- mousedown and up code from http://jsfiddle.net/JLTVS/2/ -->
+		/* mousedown and up code from http://jsfiddle.net/JLTVS/2/ */
 		.mousedown(function() {
 			$('div.mask').show();
 		})
 		.mouseup(function() {
 			$('div.mask').hide();
-		}).draggable({containment: "parent"}).resizable({containment: "parent"});
+		})
+		.draggable({containment: "parent"})
+		.resizable({containment: "parent"});
 
+		/* close button code */
 		$( "button.closeButton" )
-		.mousedown(function() {
+		.click(function() {
 			this.parentNode.parentNode.remove();
 		});
-		
-        $.contextMenu({
-            selector: '.context-menu-one', 
-            callback: function(key, options) {
-                if(key == "bl")
+
+		/* context menu for moving videos around the screen */
+		$.contextMenu({
+			selector: '.context-menu-one', 
+			callback: function(key, options) {
+				if(key == "bl")
 				{
 					this.parent().css({'width': '50%', 'height': '50%', 'top': 'auto', 'right': 'auto', 'bottom': '0px', 'left': '0px'});
 				}
-                if(key == "br")
+				if(key == "br")
 				{
 					this.parent().css({'width': '50%', 'height': '50%', 'top': 'auto', 'right': '0px', 'bottom': '0px', 'left': 'auto'});
 				}
-                if(key == "tl")
+				if(key == "tl")
 				{
 					this.parent().css({'width': '50%', 'height': '50%', 'top': '0px', 'right': 'auto', 'bottom': 'auto', 'left': '0px'});
 				}
-                if(key == "tr")
+				if(key == "tr")
 				{
 					this.parent().css({'width': '50%', 'height': '50%', 'top': '0px', 'right': '0px', 'bottom': 'auto', 'left': 'auto'});
 				}
-                if(key == "fs")
+				if(key == "fs")
 				{
 					this.parent().css({'width': '100%', 'height': '100%', 'top': 'auto', 'right': 'auto', 'bottom': '0px', 'left': '0px'});
 				}
-            },
-            items: {
-                "bl": {name: "Bottom Left"},
-                "br": {name: "Bottom Right"},
-                "tl": {name: "Top Left"},
-                "tr": {name: "Top Right"},
-                "sep1": "---------",
-                "fs": {name: "Full Screen"}
-            }
-        });
+			},
+			items: {
+				"bl": {name: "Bottom Left"},
+				"br": {name: "Bottom Right"},
+				"tl": {name: "Top Left"},
+				"tr": {name: "Top Right"},
+				"sep1": "---------",
+				"fs": {name: "Full Screen"}
+			}
+		});
 	});
-});
-
-<!-- clock code https://www.sitepoint.com/create-jquery-digital-clock-jquery4u/ -->
-function updateClock ( )
-{
-	var currentTime = new Date ( );
-	var currentHours = currentTime.getHours ( );
-	var currentMinutes = currentTime.getMinutes ( );
-	var currentSeconds = currentTime.getSeconds ( );
-
-	// Pad the minutes and seconds with leading zeros, if required
-	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-	// Choose either "AM" or "PM" as appropriate
-	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-	// Convert the hours component to 12-hour format if needed
-	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-	// Convert an hours component of "0" to "12"
-	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
-	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-	$("#clock").html(currentTimeString);
- }
-
-$(document).ready(function()
-{
-   setInterval('updateClock()', 1000);
 });
