cafcecea1eec68474b316ee4dee52b03c9413af3
ellisborealis <townsend08002@comcast.net>
2016-06-23 13:45:14 -0400

    using 24 hour time now, added date and year as well formatted for better visibility on the projector

--- js/digitalClock.js
+++ js/digitalClock.js
@@ -1,4 +1,4 @@
-<!-- clock code https://www.sitepoint.com/create-jquery-digital-clock-jquery4u/ -->
+/* clock code https://www.sitepoint.com/create-jquery-digital-clock-jquery4u/ */
 function updateClock ( )
 {
 	var currentTime = new Date ( );
@@ -10,17 +10,8 @@ function updateClock ( )
 	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
 	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-	// Choose either "AM" or "PM" as appropriate
-	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-	// Convert the hours component to 12-hour format if needed
-	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-	// Convert an hours component of "0" to "12"
-	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
 	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	var currentTimeString = currentTime.toDateString() + ", " + currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
 	$("#clock").html(currentTimeString);
  }
